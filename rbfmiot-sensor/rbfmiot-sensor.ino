f775af85cd42b870e0bd779f423b148ce23e09e5
Sergey Vasiliev <brighterr@gmail.com>
2016-12-20 23:56:03 +0300

    MQTT pub/sub functionality development in progress

--- rbfmiot-sensor/rbfmiot-sensor.ino
+++ rbfmiot-sensor/rbfmiot-sensor.ino
@@ -1,15 +1,22 @@
 #include<ESP8266WiFi.h>
 #include <RBFMIOT_BME280.h>
+#include "PubSubClient.h"
 
 extern "C" {
   #include "user_interface.h"
 }
 
 const char* ssid = "IoT";
-const char* password = "********";
+//const char* password = "********";
+
+const char *mqttServerName="";
+const uint8_t mqttServerPort = 1883;
 char macAddr[12];
 
 RBFMIOT_BME280 rbfmiotBme280;
+
+void readMACaddr(char *macAddr);
+void initWiFi();
 
 void setup() {
 //  int8_t id;
@@ -19,6 +26,7 @@ void setup() {
   Serial.println();
 //  Serial.println("Configure start...");
   rbfmiotBme280.configure(SDA_PIN, SCL_PIN);
+  initWiFi();
 //  Serial.println("Configure complete...");
 //  rbfmiotBme280.readId(&id);
 //  Serial.print("id 0x");
@@ -50,7 +58,23 @@ void readMACaddr(char *macAddr) {
   }
 }
 
+void initWiFi() {
+  delay(10);
+  Serial.println();
+  Serial.println("WiFi connect start...");
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println();
+  Serial.print("WiFi connected. IP address ");
+  Serial.println(WiFi.localIP());
+  Serial.println("WiFi connect complete...");
+}
+
 //TODO: implement this
 void suspendSensor() {
 }
+
 
