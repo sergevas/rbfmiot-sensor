a6ef15e3f4b9ffd4f2da9d1a8f58ecf34793e857
Sergey Vasiliev <brighterr@gmail.com>
2017-01-02 17:00:55 +0300

    Development in progress: MQTT functionality

--- rbfmiot-sensor/rbfmiot-sensor.ino
+++ rbfmiot-sensor/rbfmiot-sensor.ino
@@ -8,103 +8,115 @@ extern "C" {
 
 const char* ssid = "IoT";
 const char* password = "********";
-const char *mqttServerName="iot.eclipse.org";
-const uint8_t mqttServerPort = 1883;
+
+const char *mqttServerName="192.168.1.80";
+const uint16_t mqttServerPort = 1883;
 const char* mqttUsername = "rbfmiotUser";
 const char* mqttPassword = "rbfmiotPasswd";
+
 const char* statusOn = "on";
+const char* statusSuspend = "suspend";
 const char* statusOff = "off";
+
+const String getidCmd = "getid";
+const String activateCmd = "activate";
+const String suspendCmd = "suspend";
+
 const int willQoS = 2;
 const boolean willRetain = true;
 char macAddr[12];
-char statusTopic[23];
-char controlTopic[24];
-char telemTopicTemperature[28];
-char telemTopicPressure[25];
-char telemTopicHumidity[25];
+char statusTopic[24];
+char requestTopic[25];
+char replyTopic[23];
+char telemTopicTemperature[29];
+char telemTopicPressure[26];
+char telemTopicHumidity[26];
 double temp, pres, hum;
-byte measData[8];
+//byte measData[8];
 
 RBFMIOT_BME280 rbfmiotBme280;
 WiFiClient wifiClient;
-PubSubClient pubSubClient(wifiClient);
+PubSubClient pubSubClient(mqttServerName, mqttServerPort, wifiClient);
 
-void readMACaddr(char *macAddr);
+String readMACaddr();
+void double2byteArr(double srcVal, byte *destVal);
 void initWiFi();
+void reconnect();
+void callback(char *topic, byte *payload, unsigned int length);
+void handleCommand(char *command);
 
 void setup() {
-//  int8_t id;
-  
+  String macAddrStr = readMACaddr();
   Serial.begin(115200);
   Serial.println();
   Serial.println();
-//  Serial.println("Configure start...");
+  Serial.println("Configure start...");
   rbfmiotBme280.configure(SDA_PIN, SCL_PIN);
   initWiFi();
-//  rbfmiotBme280.readId(&id);
-//  Serial.print("id 0x");
-//  Serial.println(id, HEX);
-//  Serial.println("Reading device id complete...");
-  readMACaddr(macAddr);
-  sprintf(statusTopic, "env/%s/status", macAddr);
-  sprintf(statusTopic, "env/%s/control", macAddr);
-  sprintf(telemTopicPressure, "env/%s/pressure", macAddr);
-  sprintf(telemTopicHumidity, "env/%s/humidity", macAddr);
-  sprintf(telemTopicTemperature, "env/%s/temperature", macAddr);
-  delay(50);
+  macAddrStr.toCharArray(macAddr, 12);
+  (String("env/" + macAddrStr + "/pressure")).toCharArray(telemTopicPressure, 26);
+  (String("env/" + macAddrStr + "/humidity")).toCharArray(telemTopicHumidity, 26);
+  (String("env/" + macAddrStr + "/temperature")).toCharArray(telemTopicTemperature, 29);
+  (String("env/" + macAddrStr + "/request")).toCharArray(requestTopic, 25);
+  (String("env/" + macAddrStr + "/reply")).toCharArray(replyTopic, 23);
+  (String("env/" + macAddrStr + "/status")).toCharArray(statusTopic, 24);
   Serial.println("Got topic names...");
   Serial.println(statusTopic);
-  Serial.println(controlTopic);
+  Serial.println(requestTopic);
+  Serial.println(replyTopic);
   Serial.println(telemTopicTemperature);
   Serial.println(telemTopicPressure);
   Serial.println(telemTopicHumidity);
-  pubSubClient.setServer(mqttServerName, mqttServerPort);
-//  Serial.println("Configure complete...");
+  pubSubClient.setCallback(callback);
+  Serial.println("Configure complete...");
 }
 
 void loop() {
+  char *measVal;
   if (!pubSubClient.connected()) {
     reconnect();
   }
   pubSubClient.loop();
   rbfmiotBme280.readAll(&temp, &pres, &hum);
-  
   Serial.print("Publish temp=");
   Serial.print(temp);
-  double2byteArr(temp, measData);
-  pubSubClient.publish(telemTopicTemperature, measData, 8);
+  measVal = getValAsString(temp);
+  pubSubClient.publish(telemTopicTemperature, measVal);
+  free(measVal);
   Serial.print("\tpres=");
   Serial.print(pres);
-  double2byteArr(pres, measData);
-  pubSubClient.publish(telemTopicPressure, measData, 8);
+  measVal = getValAsString(pres);
+  pubSubClient.publish(telemTopicPressure, measVal);
+  free(measVal);
   Serial.print("\thum=");
   Serial.print(hum);
-  double2byteArr(hum, measData);
-  pubSubClient.publish(telemTopicHumidity, measData, 8);
+  measVal = getValAsString(hum);
+  pubSubClient.publish(telemTopicHumidity, measVal);
+  free(measVal);
   Serial.println();
   delay(2000);
 }
 
-void readMACaddr(char *macAddr) {
-  int i;
-  uint8_t macBin[6];
-  WiFi.macAddress(macBin);
-  for (i = 0; i < sizeof(macBin); i++) {
-    sprintf(macAddr,"%s%02x", macAddr, macBin[i]);
-  }
+char *getValAsString(double aVal) {
+  String strVal = String(aVal, 2);
+  int strValLength = strVal.length() + 1;
+  char *strValArr = (char*)malloc(strValLength);
+  strVal.toCharArray(strValArr, strValLength);
+  return strValArr;
 }
 
-void double2byteArr(double srcVal, byte *destVal) {
-  union {
-    double aSrcVal;
-    byte aDestVal[8];
-  } double2byteArrUnion;
-  double2byteArrUnion.aSrcVal = srcVal;
-  memcpy(destVal, double2byteArrUnion.aDestVal, 8);
+String readMACaddr() {
+  int i;
+  uint8_t macBin[6];
+  String macStr = "";
+  WiFi.macAddress(macBin);
+  for (i = 0; i < 6; i++) {
+    macStr += String(macBin[i], HEX);
+  }
+  return macStr;
 }
 
 void initWiFi() {
-  delay(10);
   Serial.println();
   Serial.println("WiFi connect start...");
   WiFi.begin(ssid, password);
@@ -124,7 +136,7 @@ void reconnect() {
     if (pubSubClient.connect(macAddr, mqttUsername, mqttPassword, statusTopic, willQoS, willRetain, statusOff)) {
       Serial.println("Connected to MQTT broker...");
       pubSubClient.publish(statusTopic, statusOn, true);
-      pubSubClient.subscribe(controlTopic, 1);
+      pubSubClient.subscribe(requestTopic, 1);
     } else {
       Serial.print("Unable to connect to MQTT broker! rc=");
       Serial.println(pubSubClient.state());
@@ -134,9 +146,29 @@ void reconnect() {
   }
 }
 
+void callback(char *topic, byte *payload, unsigned int length) {
+  int i;
+  String command = "";
+  for (i = 0; i < length; i++) {
+    command += String((char)payload[i]);
+  }
+  handleCommand(command);
+}
+
+void handleCommand(String command) {
+  Serial.print("command=");
+  Serial.println(command);
+  if (getidCmd.equals(command)) {
+    int8_t id;
+    char idArr[3];
+    rbfmiotBme280.readId(&id);
+    String idStr = String(id, HEX);
+    idStr.toCharArray(idArr, 3);
+    pubSubClient.publish(replyTopic, idArr);
+  }
+}
 
 //TODO: implement this
 void suspendSensor() {
 }
-
 
